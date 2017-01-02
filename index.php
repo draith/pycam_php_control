cb7bb620a06350c401c66c83505007cfbc04b4ac
Douglas Raith <draith2001@gmail.com>
2017-01-02 21:01:54 +0000

    https, curl, no iframe

--- index.php
+++ index.php
@@ -1,29 +1,27 @@
 <?php
 putenv("TZ=Europe/London");
 error_reporting(E_ALL);
-ini_set('display_errors', '1');
+// Ensure https connection...
+if ($_SERVER["SERVER_PORT"] != 443) {
+    $redir = "Location: https://" . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+    header($redir);
+    exit();
+}
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
+  <style>
+    h2, button { font-size: 40px; margin: 5px;}
+    div { font-size: 30px }
+    div.hidden { display: none; position: relative; left: 50px; }
+  </style>
   <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
    <title>Pycam</title>
-   <style>
-   iframe {
-	   width: 220px;
-	   height: 60px;
-	   position: relative;
-	   top: 10px;
-	   overflow: hidden; 
-	}
-	h2,button { font-size: 40px; margin: 5px; }
-	div { font-size: 30px }
-	div.hidden { display: none; position: relative; left: 50px; }
-   </style>
    <script>
    function switchOnOff()
    {
-	   top.document.getElementById('iframe').setAttribute('src','sendCommand.php?command=Switch');
+	   window.location.href = ".?command=Switch";
    }
    function toggleVis(id)
    {
@@ -46,11 +44,30 @@ if (isset($_GET['del']))
 	// Delete files matching wildcard string.
 	array_map('unlink', glob($_GET['del']));
 }
-echo "<h2>Status: <iframe height='35' width='80' src='sendCommand.php?command=Status' id='iframe'></iframe>";
-echo " <button onclick = 'switchOnOff()'>Start/Stop</button></h2>\n";
+
+// Get camera status, with switching if command is set.
+$key = file_get_contents('key.pub');
+$pi_address = file_get_contents("lastip.txt");
+$command = isset($_GET['command']) ? $_GET['command'] : "Status";
+openssl_public_encrypt($command  . "/" . time(), $crypted, $key, OPENSSL_PKCS1_OAEP_PADDING);
+$encoded = base64_encode($crypted);
+$curl = curl_init("http://$pi_address/$encoded");
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$curl_response = curl_exec($curl);
+if ($curl_response === false) {
+  $info = curl_getinfo($curl);
+  curl_close($curl);
+  echo 'error occured during curl exec. Additional info: ' . var_export($info);
+}
+curl_close($curl);
+$running = (strcmp($curl_response, "Running") == 0);
+// Display status and toggle button.
+echo "<h2>Status: <span style='color:" . ($running ? "green" : "red") . "'>$curl_response</span> ";
+echo "<button onclick = 'switchOnOff()'>" . ($running ? "Stop" : "Start") . "</button></h2>\n";
+
 $image = "snapshot.jpg";
 echo "<div>Snapshot uploaded " . date('D, d M, H:i:s', filemtime($image));
-echo "<br><img src=\"$image\" style='max-width:50%; height:auto'>" ; 
+echo "<br><img src=\"$image\" style='max-width:50%; height:auto'>";
 
 // get all videos to list
 $videos = glob('*.mp4', GLOB_BRACE);
