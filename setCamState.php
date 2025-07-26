c68aa7a5aca073b89b3df5252f198a36260a8b7f
draith <draith@users.noreply.github.com>
2025-07-26 18:16:38 +0100

    replaced file_put_contents

--- setCamState.php
+++ setCamState.php
@@ -2,7 +2,8 @@
 header("Content-Type: text/plain");
 if ($_GET["id"] === "mypicam") // TODO: hide/encrypt?
 {
-    file_put_contents("camState.txt", $_GET["state"]);    
+    $file = fopen("camState.txt", "w");
+    fwrite($file, $_GET["state"]);    
     echo "OK";
 }
 else
